b8a4636484a75391e54991fe7a0698253a0c76e6
matheus-0063 <matheus.fer.0063@gmail.com>
2026-07-07 22:47:16 -0300

    fix: corrige AppointemntService

--- backend/src/MeuBarbeiro.Domain/Entities/Review.cs
+++ backend/src/MeuBarbeiro.Domain/Entities/Review.cs
@@ -3,10 +3,10 @@ namespace MeuBarbeiro.Domain.Entities;
 public sealed class Review
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
-    public Guid AppointmentId { get; private set; }
-    public Guid ClientId { get; private set; }
-    public Guid BarberId { get; private set; }
-    public Guid BarbershopId { get; private set; }
-    public int Stars { get; private set; }
-    public DateTime CreatedAtUtc { get; private set; } = DateTime.UtcNow;
+    public Guid AppointmentId { get; set; }
+    public Guid ClientId { get; set; }
+    public Guid BarberId { get; set; }
+    public Guid BarbershopId { get; set; }
+    public int Stars { get; set; }
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
 }
